be73cb8bea89fd6ce1c22463c4d9fb2759c5c7cd
rafaellabandeira <rafabansko@gmail.com>
2026-01-19 10:50:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Selector de idioma (placeholder, se puede ampliar)
+document.getElementById('language').addEventListener('change', function() {
+    const lang = this.value;
+    alert('Cambiar idioma a: ' + lang + ' (funcionalidad aún no implementada)');
+});
 
