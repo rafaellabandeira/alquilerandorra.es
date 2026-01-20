7301491162a8e02fe493d483c18c8384de0415a5
rafaellabandeira <rafabansko@gmail.com>
2026-01-20 17:23:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-// Selector de idioma (placeholder, se puede ampliar)
-document.getElementById('language').addEventListener('change', function() {
-    const lang = this.value;
-    alert('Cambiar idioma a: ' + lang + ' (funcionalidad aún no implementada)');
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let counter = 0;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(0px)';
+
+nextBtn.addEventListener('click', () => {
+    if (counter >= carouselImages.length - 1) counter = -1;
+    counter++;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
 });
 
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) counter = carouselImages.length;
+    counter--;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+});
+
+setInterval(() => {
+    if (counter >= carouselImages.length - 1) counter = -1;
+    counter++;
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+}, 5000);
